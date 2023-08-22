33a172b8e2957f5ce53182e6d4e4cd3457791128
samuelselasi <samelsekasi@gmail.com>
2023-08-22 15:17:11 +0000

    Modify chapters router files to pep8 standard

--- backend/app/routers/chapters/database.py
+++ backend/app/routers/chapters/database.py
@@ -1,12 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines PostgreSQL database URL"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Define your PostgreSQL database URL
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:sel@localhost/test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
--- backend/app/routers/chapters/main.py
+++ backend/app/routers/chapters/main.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
+"""Module that defines endpoints for chapters"""
+
 from typing import List
-
-from fastapi import Depends, FastAPI, HTTPException, APIRouter
+from fastapi import Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
-
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
@@ -21,18 +22,31 @@ def get_db():
 
 
 @router.post("/chapters/", response_model=schemas.Chapter)
-async def create_chapter_for_country(
-    country_id: int, chapter: schemas.ChapterCreate, db: Session = Depends(get_db)):
+async def create_chapter_for_country(country_id: int,
+                                     chapter: schemas.ChapterCreate,
+                                     db: Session = Depends(get_db)):
+    """Function that defines endpoint to create a chapter for a country"""
+
     return crud.create_chapter(db=db, chapter=chapter, country_id=country_id)
 
+
 @router.get("/chapters/", response_model=List[schemas.Chapter])
-async def read_chapters_by_country(
-    country_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    chapters = crud.get_chapters_by_country(db, country_id=country_id, skip=skip, limit=limit)
+async def read_chapters_by_country(country_id: int,
+                                   skip: int = 0,
+                                   limit: int = 100,
+                                   db: Session = Depends(get_db)):
+    """Function that defines endpoint to get chapters based on country_id"""
+
+    chapters = crud.get_chapters_by_country(db,
+                                            country_id=country_id,
+                                            skip=skip, limit=limit)
     return chapters
+
 
 @router.get("/chapters/{chapter_id}", response_model=schemas.Chapter)
 async def read_chapter(chapter_id: int, db: Session = Depends(get_db)):
+    """Function that defines endpoint to get chapetr based on id"""
+
     db_chapter = crud.get_chapter(db, chapter_id=chapter_id)
     if db_chapter is None:
         raise HTTPException(status_code=404, detail="Chapter not found")
@@ -40,13 +54,17 @@ async def read_chapter(chapter_id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/chapters/{chapter_id}", response_model=schemas.Chapter)
-async def update_chapter(
-    chapter_id: int, chapter_update: schemas.ChapterBase, db: Session = Depends(get_db)
-):
+async def update_chapter(chapter_id: int,
+                         chapter_update: schemas.ChapterBase,
+                         db: Session = Depends(get_db)):
+    """Function that defines endpoint to update a chapter by id"""
+
     return crud.update_chapter(db, chapter_id, chapter_update)
 
 
 @router.delete("/chapters/{chapter_id}", response_model=None)
 async def delete_chapter(chapter_id: int, db: Session = Depends(get_db)):
+    """Function that defines endpoint to delete a chapter by id"""
+
     crud.delete_chapter(db, chapter_id)
     return {"message": "Chapter deleted successfully"}
