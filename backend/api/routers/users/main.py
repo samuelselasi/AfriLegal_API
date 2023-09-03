9bfe9339bf168230be35794323efe2766c8b0a7c
samuelselasi <samelsekasi@gmail.com>
2023-09-03 20:08:52 +0000

    MOdify files to pep8 standard

--- backend/api/routers/users/main.py
+++ backend/api/routers/users/main.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
+"""Module that defines endpoints for items"""
+
 from typing import List
-
-from fastapi import Depends, FastAPI, HTTPException, APIRouter
 from sqlalchemy.orm import Session
-
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+from fastapi import Depends, HTTPException, APIRouter
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -21,29 +22,31 @@ async def get_db():
 
 
 @router.post("/users/", response_model=schemas.User)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+async def create_user(user: schemas.UserCreate,
+                      db: Session = Depends(get_db)):
+    """Endpoint to create a user"""
+
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(
+                status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
 
 @router.get("/users/", response_model=List[schemas.User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_users(skip: int = 0, limit: int = 100,
+                     db: Session = Depends(get_db)):
+    """Edpoint to read all users"""
+
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
 @router.get("/users/{user_id}", response_model=schemas.User)
 async def read_user(user_id: int, db: Session = Depends(get_db)):
+    """Endpoint to read user by id"""
+
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
-
-
-@router.post("/users/{user_id}/items/", response_model=schemas.Item)
-async def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
