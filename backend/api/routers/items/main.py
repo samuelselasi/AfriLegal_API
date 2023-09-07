68af33dceb6e0b68da669ced4920c0398809b387
samuelselasi <samelsekasi@gmail.com>
2023-09-07 15:46:03 +0000

    Modify items files to PEP8 standard

--- backend/api/routers/items/main.py
+++ backend/api/routers/items/main.py
@@ -1,8 +1,6 @@
 from typing import List
-
-from fastapi import Depends, FastAPI, HTTPException, APIRouter
+from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
-
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
@@ -21,13 +19,13 @@ def get_db():
 
 
 @router.post("/users/{user_id}/items/", response_model=schemas.Item)
-async def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
+async def create_item_for_user(user_id: int, item: schemas.ItemCreate,
+                               db: Session = Depends(get_db)):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
 @router.get("/items/", response_model=List[schemas.Item])
-async def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_items(skip: int = 0, limit: int = 100,
+                     db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
